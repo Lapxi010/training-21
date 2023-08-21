cd8cad1a9dc7286c6d229fbba8b71a70b83a30da
Lapxi010 <Lapxi010@yandex.ru>
2023-08-21 18:24:59 +0300

    add buble-sort

---
+++ buble-sort.js
@@ -0,0 +1,17 @@
+function bubleSort (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
+            }
+        }
+    }
+
+    return arr
+}
+
+let arr = [2, 8, 3, 4, 5, 10, 1]
+
+console.log(bubleSort(arr));
