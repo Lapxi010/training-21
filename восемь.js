404911f983a108f12d5b29b16e120db732667e4c
Lapxi010 <Lapxi010@yandex.ru>
2023-08-21 21:45:21 +0300

    ad

---
+++ восемь.js
@@ -0,0 +1,28 @@
+class LinkedList {
+    constructor (value, next) {
+        this.value = value
+        this.next = next
+    }
+}
+
+function reverceLinkedList (node) {
+    let dummy = node
+    let prev = null 
+
+    while (dummy) {
+        let tmp = dummy.next
+        dummy.next = prev
+        prev = dummy
+        dummy = tmp 
+    }
+
+    return prev
+}
+
+let one = new LinkedList(1, null);
+let two = new LinkedList(2, null);
+let three = new LinkedList(3, null);
+
+one.next = two
+two.next = three
+console.log(reverceLinkedList(one))
