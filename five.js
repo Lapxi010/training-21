8f70562bd87f94bff761096b4ec394f8a35cf76f
Lapxi010 <Lapxi010@yandex.ru>
2023-08-21 21:39:17 +0300

    er

--- five.js
+++ five.js
@@ -1,25 +1,56 @@
-class Three {
-    constructor(value, children) {
-        this.value = value
-        this.children = children
-    }
-}
-
 class TwoArray {
-    constructor (value, prev, next) {
+    constructor (children, value, prev, next) {
+        this.children = children
         this.value = value
         this.prev = prev
         this.next = next
     }
-
-    pop () {
-        
-    } 
 }
+
+class Queue {
+    constructor () {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    add (el) {
+        let tmp = new TwoArray(el.children, el.value, this.tail, null)
+        if (this.head === null) {
+            this.head = tmp;
+        }
+        this.tail = tmp
+        this.length += 1
+    };
+
+    shift () {
+        if (this.length === 0) {
+            throw new Error('Так нельзя')
+        }
+        let tmp = this.head 
+
+        if (this.head.next === null) {
+            this.head = this.tail
+        } else {
+            this.head = this.head.next
+        }
+        if (this.length > 0) {
+            this.length -= 1
+        }
+
+        return tmp
+    };
+
+    length () {
+        return length
+    }
+};
 
 function bfs (three, cb) {
     if (three) {
-        let queue = [three];
+        let queue = new Queue();
+
+        queue.add(three)
         
         while (queue.length !== 0) {
             let level = queue.length;
@@ -32,10 +63,30 @@ function bfs (three, cb) {
                 }
                 
                 cb(node.value)
-
-                queue.push(...node.children);
+                if (!node.children) {
+                    continue
+                }
+                let l = node.children.length
+                for (let i = 0; i < l; i++) {
+                    queue.add(node.children[i]);
+                }
             }
         }
     }
 }
 
+class Three {
+    constructor (value, children) {
+        this.value = value
+        this.children = children
+    }
+}
+
+let three = new Three (1, null)
+let three1 = new Three(2, null)
+let three2 = new Three(3, null)
+let three3 = new Three(4, null)
+three.children = [three1, three2]
+three2.children = [three3]
+
+bfs(three, (el) => console.log(el))
---
+++ seven.js
@@ -0,0 +1,27 @@
+function er (n) {
+    if (n === 0 || n === 1) {
+        return 0
+    }
+
+    let arr = [];
+
+    for (let i = 2; i < n + 1; i++) {
+        arr.push(i);
+    }
+
+    for (let i = 2; i < n + 1; i++) {
+        for (let j = i; j < n; j++) {
+            if (arr[j] === null) {
+                continue
+            }
+    
+            if (arr[j] % i === 0) {
+                arr[j] = null
+            }
+        }
+    }
+    
+    return arr.reduce((acc, el) => el !== null ? acc += 1 : acc, 0)
+}
+
+console.log(er(9))
